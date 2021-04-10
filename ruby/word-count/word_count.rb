0bcaf4a0811c62759d67561a7618fe50a4f731e9
wfswee <wfswee@chicagobooth.edu>
2021-04-10 20:54:58 +0000

    not working

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -6,3 +6,23 @@ To get started with TDD, see the `README.md` file in your
 `ruby/word-count` directory.
 =end
 
+# For example for the input `"olly olly in come free"`
+
+# ```text
+# olly: 2
+# in: 1
+# come: 1
+# free: 1
+# ```
+# assert_equal "variable_1", "variable_1 = 42"[/\w+/]
+
+class Phrase
+    def self.word_count(a_phrase)
+     p a_phrase
+        #  words = a_phrase.to_s
+        #.split[/\w+/]
+        #p words
+    end
+end
+
+Phrase.word_count(asdf)
