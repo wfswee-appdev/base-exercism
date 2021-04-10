8c8ed1f43857c05e1d58739bd722f5a5826ca4dc
wfswee <wfswee@chicagobooth.edu>
2021-04-10 20:01:49 +0000

    still not working

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -5,4 +5,24 @@ Write your code for the 'Raindrops' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/raindrops` directory.
 =end
-#asdf
+
+# - If the number has 3 as a factor, output 'Pling'.
+# - If the number has 5 as a factor, output 'Plang'.
+# - If the number has 7 as a factor, output 'Plong'.
+# - If the number does not have 3, 5, or 7 as a factor,
+#   just pass the number's digits straight through.
+
+class Raindrops
+    def convert(number)
+        answer = ""
+        number = number.to_i
+        if number % 3 == 0 then answer += "Pling"
+        if number % 5 == 0 then answer += "Plang"
+        if number % 6 == 0 then answer += "Plong"
+        else answer << number
+            return answer
+        end
+    end
+    end
+end
+end
