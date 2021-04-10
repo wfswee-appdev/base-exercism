98372572e4fc75df6ea427083d7111ad23848095
wfswee <wfswee@chicagobooth.edu>
2021-04-10 20:22:36 +0000

    finished

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -12,17 +12,17 @@ To get started with TDD, see the `README.md` file in your
 # - If the number does not have 3, 5, or 7 as a factor,
 #   just pass the number's digits straight through.
 
+
 class Raindrops
     def self.convert(number)
         answer = ""
-        number = number.to_i
-        if number % 3 == 0 then answer += "Pling"
-        if number % 5 == 0 then answer += "Plang"
-        if number % 6 == 0 then answer += "Plong"
-        else answer << number
-            return answer
-        end
-    end
+        if number % 3 == 0 then answer += "Pling" end
+        if number % 5 == 0 then answer += "Plang" end
+        if number % 7 == 0 then answer += "Plong" end
+        if answer == "" then answer += number.to_s end
+
+            p answer
     end
 end
-end
+
+Raindrops.convert(1)
