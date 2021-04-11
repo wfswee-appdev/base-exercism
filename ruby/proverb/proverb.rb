c12a5fd9679ee6d4f51ef0f218dbab40b593ad12
wfswee <wfswee@chicagobooth.edu>
2021-04-11 21:27:31 +0000

    output looks good but not passing

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -7,17 +7,23 @@ To get started with TDD, see the `README.md` file in your
 =end
 
 class Proverb
-  def initialize(*args)
+  def initialize(*args, qualifier: nil)
     final_word = *args[0]
 
     proverb = *args.each_cons(2).map { |first, second|
       "For want of a #{first} the #{second} was lost.\n"
     }
 
-    puts proverb << "And all for the want of a #{final_word[0]}.\n"
+    puts proverb << "And all for the want of a #{qualifier} #{final_word[0]}."
   end
 end
 
-Proverb.new("nail", "shoe")
-# expected = "For want of a nail the shoe was lost.\n" \
-#   'And all for the want of a nail.'
+    chain = %w(nail shoe horse rider message battle kingdom)
+    Proverb.new(*chain, qualifier: 'horseshoe')
+    # expected = "For want of a nail the shoe was lost.\n" \
+    #   "For want of a shoe the horse was lost.\n" \
+    #   "For want of a horse the rider was lost.\n" \
+    #   "For want of a rider the message was lost.\n" \
+    #   "For want of a message the battle was lost.\n" \
+    #   "For want of a battle the kingdom was lost.\n" \
+    #   'And all for the want of a horseshoe nail.'
