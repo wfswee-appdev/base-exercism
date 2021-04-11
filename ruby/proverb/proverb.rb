2a21c5d35a9fe916a8ed8c63f4e44c929d2e1014
wfswee <wfswee@chicagobooth.edu>
2021-04-11 04:50:03 +0000

    starting

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -6,3 +6,19 @@ To get started with TDD, see the `README.md` file in your
 `ruby/proverb` directory.
 =end
 
+class Proverb
+    def initialize(word_list)
+        @word_list = word_list
+    end
+
+    def write_proverb
+        @word_list = "For want of a"
+    end
+
+
+end
+
+    proverb = Proverb.new('nail', 'shoe')
+    expected = "For want of a nail the shoe was lost.\n" \
+
+["nail", "shoe", "horse", "rider", "message", "battle", "kingdom"]
