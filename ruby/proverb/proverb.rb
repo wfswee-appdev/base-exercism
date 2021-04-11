ea6a15494d098d2ae57e3d1ca9be862dacaa121b
wfswee <wfswee@chicagobooth.edu>
2021-04-11 20:41:18 +0000

    ask why this one doesn't work

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -6,25 +6,23 @@ To get started with TDD, see the `README.md` file in your
 `ruby/proverb` directory.
 =end
 
-class Proverb
+class Proverb    
     def initialize(*args)
         @word_list = *args
     end
 
     def write_the_proverb
-        @word_list.map { |first, second| "For want of a #{first} the #{second} was lost.\n" }
-        end
-        p @word_list
+        proverb = @word_list.map { |first, second|
+        "For want of a #{first} the #{second} was lost.\n" }
+        p proverb
     end
-  
-    def last_line
-        "And all for the want of a #{@word_list.first}."
-    end
-
-    write_the_proverb << last_line
-
+    # def last_line
+    #     "And all for the want of a #{@word_list.first}."
+    # end
 end
 
-Proverb.new("nail", "shoe", "horse", "rider")
+      #  ["nail", "shoe", "horse", "rider"].map { |first, second| "For want of a #{first} the #{second} was lost.\n" }
 
-# ["nail", "shoe", "horse", "rider", "message", "battle", "kingdom"]
+p Proverb.new("nail", "shoe", "horse", "rider")
+
+
